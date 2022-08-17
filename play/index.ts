2b45f85639b2f69cb347a70a2cf9af98e87222cc
Pyrolistical <pyrogx1133@gmail.com>
2022-08-17 12:09:54 -0700

    extracted puzzle generator into its own file

--- play/index.ts
+++ play/index.ts
@@ -8,18 +8,10 @@ import "../polyfill.js";
 import { v4 as uuid } from "uuid";
 import Seedrandom from "seedrandom";
 
-import {
-	Main,
-	Coordinate,
-	CoordinateKey,
-	asCoordinateKey,
-	Grid,
-	Cell,
-	Cells,
-	Connection,
-} from "./elements";
+import { Main, Grid, Cell, Cells } from "./elements";
 
 import { html } from "./component";
+import PuzzleGenerator from "./puzzle-generator";
 
 const width = 1000;
 const height = (width * Math.sqrt(3)) / 2;
@@ -31,161 +23,6 @@ const size = 15;
 
 const validate = (grid: Grid, cell: Cell) => {};
 
-const normalizeConnections = (connection: number): Connection => {
-	if (connection === 0) {
-		throw new Error("connection cannot be empty");
-	}
-	if (connection === 0b111111) {
-		return 0b111111; // star
-	}
-	while (!(connection & 0b100000) || connection & 0b000001) {
-		const first = connection & 0b100000;
-		connection <<= 1;
-		connection &= 0b111111;
-		if (first) {
-			connection |= 0b000001;
-		}
-	}
-	const truncated = (connection & 0b011110) >> 1;
-	switch (truncated) {
-		case 0b0000:
-			return 0b100000; // i
-
-		case 0b1000:
-			return 0b110000; // v
-		case 0b0100:
-			return 0b101000; // c
-		case 0b0010:
-			return 0b100100; // l
-		case 0b0001:
-			return 0b101000; // c
-
-		case 0b1100:
-			return 0b111000; // e
-		case 0b0110:
-			return 0b101100; // y
-		case 0b0011:
-			return 0b110100; // λ
-		case 0b1001:
-			return 0b101100; // y
-		case 0b1010:
-			return 0b110100; // λ
-		case 0b0101:
-			return 0b101010; // tri
-
-		case 0b1110:
-			return 0b111100; // half
-		case 0b0111:
-			return 0b101110; // rake
-		case 0b1011:
-			return 0b110110; // x
-		case 0b1101:
-			return 0b101110; // rake
-
-		case 0b1111:
-			return 0b111110; // hat
-
-		default:
-			throw new Error(
-				`unhandled truncated case ${truncated.toString(
-					2
-				)} from connection ${connection.toString(2)}`
-			);
-	}
-};
-
-const coordinates: Coordinate[] = [];
-for (let q = -size; q <= size; q++) {
-	for (let r = -size; r <= size; r++) {
-		const s = -q - r;
-		if (-size <= s && s <= size) {
-			coordinates.push({
-				q,
-				r,
-				s,
-			});
-		}
-	}
-}
-
-type Direction = 0 | 1 | 2 | 3 | 4 | 5;
-function* Neighbours({ q, r, s }: Coordinate): Generator<
-	Coordinate & {
-		direction: Direction;
-	},
-	any,
-	any
-> {
-	yield {
-		q,
-		r: r - 1,
-		s: s + 1,
-		direction: 0,
-	};
-	yield {
-		q: q + 1,
-		r: r - 1,
-		s,
-		direction: 1,
-	};
-	yield {
-		q: q + 1,
-		r,
-		s: s - 1,
-		direction: 2,
-	};
-	yield {
-		q,
-		r: r + 1,
-		s: s - 1,
-		direction: 3,
-	};
-	yield {
-		q: q - 1,
-		r: r + 1,
-		s,
-		direction: 4,
-	};
-	yield {
-		q: q - 1,
-		r,
-		s: s + 1,
-		direction: 5,
-	};
-}
-function* ValidNeighbours(
-	size: number,
-	coordinate: Coordinate
-): Generator<
-	Coordinate & {
-		direction: Direction;
-	},
-	any,
-	any
-> {
-	for (const { q, r, s, direction } of Neighbours(coordinate)) {
-		if (q < -size || q > size) {
-			continue;
-		}
-		if (r < -size || r > size) {
-			continue;
-		}
-		if (s < -size || s > size) {
-			continue;
-		}
-		yield { q, r, s, direction };
-	}
-}
-function skip<T>(n: number, iterator: IterableIterator<T>): IteratorResult<T> {
-	let cursor: IteratorResult<T> = iterator.next();
-	while (n-- > 0) {
-		cursor = iterator.next();
-	}
-	return cursor!;
-}
-
-console.time("solution");
-const solutionTree: Record<CoordinateKey, number> = {};
 let seed: string;
 if (window.location.hash === "") {
 	seed = uuid();
@@ -194,58 +31,16 @@ if (window.location.hash === "") {
 	seed = window.location.hash.substring(1);
 }
 const puzzleRandom = Seedrandom(seed);
-const connected = new Set<CoordinateKey>();
-const working = new Map<CoordinateKey, Coordinate>();
-const start = coordinates[Math.floor(puzzleRandom() * coordinates.length)]!;
-connected.add(asCoordinateKey(start));
-for (const { q, r, s } of ValidNeighbours(size, start)) {
-	working.set(asCoordinateKey({ q, r, s }), { q, r, s });
-}
-
-while (connected.size < coordinates.length) {
-	if (working.size === 0) {
-		throw new Error("working set unexpectedly empty");
-	}
-	const workingIndex = Math.floor(puzzleRandom() * working.size);
-	const { value: cellKey } = skip(workingIndex, working.keys());
-	const cell = working.get(cellKey)!;
-	const connectedNeighbours: (Coordinate & { direction: Direction })[] = [];
-	for (const { q, r, s, direction } of ValidNeighbours(size, cell)) {
-		if (connected.has(asCoordinateKey({ q, r, s }))) {
-			connectedNeighbours.push({ q, r, s, direction });
-		}
-	}
-	const { q, r, s, direction } =
-		connectedNeighbours[
-			Math.floor(puzzleRandom() * connectedNeighbours.length)
-		]!;
-
-	const connectedNeighbourKey = asCoordinateKey({ q, r, s });
-	solutionTree[connectedNeighbourKey] ??= 0;
-	solutionTree[connectedNeighbourKey] |= 0b100000 >> (direction + 3) % 6;
-
-	solutionTree[cellKey] ??= 0;
-	solutionTree[cellKey] |= 0b100000 >> direction;
-
-	connected.add(cellKey);
-	for (const { q, r, s } of ValidNeighbours(size, cell)) {
-		const nextKey = asCoordinateKey({ q, r, s });
-		if (connected.has(nextKey)) {
-			continue;
-		}
-		working.set(nextKey, { q, r, s });
-	}
-	working.delete(cellKey);
-}
 
 const cells: Cells = {};
-for (const { q, r, s } of coordinates) {
-	const solution = solutionTree[asCoordinateKey({ q, r, s })]!;
-	const cell = Cell(unseeded, q, r, s, normalizeConnections(solution));
+for (const {
+	coordinate: { q, r, s },
+	connection,
+} of PuzzleGenerator(size, puzzleRandom)) {
+	const cell = Cell(unseeded, q, r, s, connection);
 
 	cells[`${q} ${r} ${s}`] = cell;
 }
-console.timeEnd("solution");
 const grid = Grid(main.element, cells, (cell, event) => {
 	if (event.buttons === 1) {
 		cell.element.classList.remove(`rotate${cell.orientation}`);
---
+++ play/puzzle-generator.ts
@@ -0,0 +1,229 @@
+import {
+	Coordinate,
+	CoordinateKey,
+	asCoordinateKey,
+	Connection,
+} from "./elements";
+
+const normalizeConnections = (connection: number): Connection => {
+	if (connection === 0) {
+		throw new Error("connection cannot be empty");
+	}
+	if (connection === 0b111111) {
+		return 0b111111; // star
+	}
+	while (!(connection & 0b100000) || connection & 0b000001) {
+		const first = connection & 0b100000;
+		connection <<= 1;
+		connection &= 0b111111;
+		if (first) {
+			connection |= 0b000001;
+		}
+	}
+	const truncated = (connection & 0b011110) >> 1;
+	switch (truncated) {
+		case 0b0000:
+			return 0b100000; // i
+
+		case 0b1000:
+			return 0b110000; // v
+		case 0b0100:
+			return 0b101000; // c
+		case 0b0010:
+			return 0b100100; // l
+		case 0b0001:
+			return 0b101000; // c
+
+		case 0b1100:
+			return 0b111000; // e
+		case 0b0110:
+			return 0b101100; // y
+		case 0b0011:
+			return 0b110100; // λ
+		case 0b1001:
+			return 0b101100; // y
+		case 0b1010:
+			return 0b110100; // λ
+		case 0b0101:
+			return 0b101010; // tri
+
+		case 0b1110:
+			return 0b111100; // half
+		case 0b0111:
+			return 0b101110; // rake
+		case 0b1011:
+			return 0b110110; // x
+		case 0b1101:
+			return 0b101110; // rake
+
+		case 0b1111:
+			return 0b111110; // hat
+
+		default:
+			throw new Error(
+				`unhandled truncated case ${truncated.toString(
+					2
+				)} from connection ${connection.toString(2)}`
+			);
+	}
+};
+
+type Direction = 0 | 1 | 2 | 3 | 4 | 5;
+function* Neighbours({ q, r, s }: Coordinate): Generator<
+	Coordinate & {
+		direction: Direction;
+	},
+	any,
+	any
+> {
+	yield {
+		q,
+		r: r - 1,
+		s: s + 1,
+		direction: 0,
+	};
+	yield {
+		q: q + 1,
+		r: r - 1,
+		s,
+		direction: 1,
+	};
+	yield {
+		q: q + 1,
+		r,
+		s: s - 1,
+		direction: 2,
+	};
+	yield {
+		q,
+		r: r + 1,
+		s: s - 1,
+		direction: 3,
+	};
+	yield {
+		q: q - 1,
+		r: r + 1,
+		s,
+		direction: 4,
+	};
+	yield {
+		q: q - 1,
+		r,
+		s: s + 1,
+		direction: 5,
+	};
+}
+function* ValidNeighbours(
+	size: number,
+	coordinate: Coordinate
+): Generator<
+	Coordinate & {
+		direction: Direction;
+	},
+	any,
+	any
+> {
+	for (const { q, r, s, direction } of Neighbours(coordinate)) {
+		if (q < -size || q > size) {
+			continue;
+		}
+		if (r < -size || r > size) {
+			continue;
+		}
+		if (s < -size || s > size) {
+			continue;
+		}
+		yield { q, r, s, direction };
+	}
+}
+
+function skip<T>(n: number, iterator: IterableIterator<T>): IteratorResult<T> {
+	let cursor: IteratorResult<T> = iterator.next();
+	while (n-- > 0) {
+		cursor = iterator.next();
+	}
+	return cursor!;
+}
+
+function* CoordinatesGenerator(size: number): Generator<Coordinate, any, any> {
+	for (let q = -size; q <= size; q++) {
+		for (let r = -size; r <= size; r++) {
+			const s = -q - r;
+			if (-size <= s && s <= size) {
+				yield {
+					q,
+					r,
+					s,
+				};
+			}
+		}
+	}
+}
+type Solution = { coordinate: Coordinate; connection: Connection };
+type ConnectedNeighbour = { coordinate: Coordinate; direction: Direction };
+function* ConnectedNeighbours(
+	size: number,
+	cell: Coordinate,
+	connected: Set<CoordinateKey>
+): Generator<ConnectedNeighbour, any, any> {
+	for (const { q, r, s, direction } of ValidNeighbours(size, cell)) {
+		if (connected.has(asCoordinateKey({ q, r, s }))) {
+			yield { coordinate: { q, r, s }, direction };
+		}
+	}
+}
+
+export default function* (
+	size: number,
+	random: () => number
+): Generator<Solution, any, any> {
+	const coordinates: Coordinate[] = [...CoordinatesGenerator(size)];
+	const solutionTree: Record<CoordinateKey, number> = {};
+
+	const connected = new Set<CoordinateKey>();
+	const working = new Map<CoordinateKey, Coordinate>();
+	const start = coordinates[Math.floor(random() * coordinates.length)]!;
+	connected.add(asCoordinateKey(start));
+	for (const { q, r, s } of ValidNeighbours(size, start)) {
+		working.set(asCoordinateKey({ q, r, s }), { q, r, s });
+	}
+
+	while (connected.size < coordinates.length) {
+		if (working.size === 0) {
+			throw new Error("working set unexpectedly empty");
+		}
+		const workingIndex = Math.floor(random() * working.size);
+		const { value: cellKey } = skip(workingIndex, working.keys());
+		const cell = working.get(cellKey)!;
+		const connectedNeighbours = [...ConnectedNeighbours(size, cell, connected)];
+		const {
+			coordinate: { q, r, s },
+			direction,
+		} = connectedNeighbours[Math.floor(random() * connectedNeighbours.length)]!;
+
+		const connectedNeighbourKey = asCoordinateKey({ q, r, s });
+		solutionTree[connectedNeighbourKey] ??= 0;
+		solutionTree[connectedNeighbourKey] |= 0b100000 >> (direction + 3) % 6;
+
+		solutionTree[cellKey] ??= 0;
+		solutionTree[cellKey] |= 0b100000 >> direction;
+
+		connected.add(cellKey);
+		for (const { q, r, s } of ValidNeighbours(size, cell)) {
+			const nextKey = asCoordinateKey({ q, r, s });
+			if (connected.has(nextKey)) {
+				continue;
+			}
+			working.set(nextKey, { q, r, s });
+		}
+		working.delete(cellKey);
+	}
+
+	for (const { q, r, s } of coordinates) {
+		const solution = solutionTree[asCoordinateKey({ q, r, s })]!;
+		yield {
+			coordinate: { q, r, s },
+			connection: normalizeConnections(solution),
+		};
+	}
+}
