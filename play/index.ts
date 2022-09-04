908cafca901e4d911029077d41ee7aab2508d39b
Pyrolistical <pyrogx1133@gmail.com>
2022-09-04 13:32:31 -0700

    implemented coloured spans

--- play/index.ts
+++ play/index.ts
@@ -103,6 +103,7 @@ type Cell = {
 		duration: Milliseconds;
 	};
 	connection: Connection;
+	color: Color;
 };
 
 export const Orientations = [0, 60, 120, 180, 240, 300] as const;
@@ -133,6 +134,10 @@ const Connections = [
 	0b111111, // star
 ] as const;
 export type Connection = typeof Connections[number];
+
+const Colors = ["none", "red", "green", "blue"] as const;
+export type Color = typeof Colors[number];
+
 type Cells = Record<CoordinateKey, Cell>;
 
 const lerp = (t: number, a: number, b: number): number => {
@@ -141,7 +146,7 @@ const lerp = (t: number, a: number, b: number): number => {
 
 const drawCell = (
 	time: DOMHighResTimeStamp,
-	{ orientation, connection }: Cell,
+	{ orientation, connection, color }: Cell,
 	pointerDown: PointerDown | undefined
 ): boolean => {
 	ctx.save();
@@ -170,15 +175,28 @@ const drawCell = (
 		}
 	} else {
 	}
-	ctx.fillStyle = cellForeground;
-	ctx.strokeStyle = cellForeground;
+	switch (color) {
+		case "none":
+			ctx.fillStyle = ctx.strokeStyle = cellForeground;
+			break;
+		case "red":
+			ctx.fillStyle = ctx.strokeStyle = red;
+			break;
+		case "green":
+			ctx.fillStyle = ctx.strokeStyle = green;
+			break;
+		case "blue":
+			ctx.fillStyle = ctx.strokeStyle = blue;
+			break;
+	}
 	switch (connection) {
 		case 0b100000 /* i */:
 			ctx.save();
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
+			ctx.beginPath();
+			ctx.moveTo(0, 0);
+			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.restore();
 
@@ -192,8 +210,8 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
+			ctx.moveTo(0, 0);
+			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((60 * Math.PI) / 180);
@@ -207,8 +225,8 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
+			ctx.moveTo(0, 0);
+			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((120 * Math.PI) / 180);
@@ -222,8 +240,8 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(-hexagonUnitHeight, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
+			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.restore();
 			break;
@@ -232,8 +250,8 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
+			ctx.moveTo(0, 0);
+			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((60 * Math.PI) / 180);
@@ -252,12 +270,12 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
+			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(-hexagonUnitHeight, 0);
+			ctx.rotate((120 * Math.PI) / 180);
+			ctx.moveTo(0, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.restore();
@@ -272,8 +290,8 @@ const drawCell = (
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
+			ctx.moveTo(0, 0);
+			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.restore();
 			break;
@@ -282,8 +300,8 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
+			ctx.moveTo(0, 0);
+			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((120 * Math.PI) / 180);
@@ -302,12 +320,7 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
@@ -327,16 +340,11 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
+			ctx.rotate((120 * Math.PI) / 180);
 			ctx.moveTo(0, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
@@ -352,22 +360,12 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
-			ctx.lineTo(hexagonUnitHeight, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((120 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.restore();
@@ -377,22 +375,12 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
-			ctx.lineTo(hexagonUnitHeight, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
@@ -407,32 +395,17 @@ const drawCell = (
 			ctx.lineWidth = 0.25;
 			ctx.lineCap = "round";
 			ctx.beginPath();
-			ctx.moveTo(hexagonUnitHeight, 0);
-			ctx.lineTo(0, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.beginPath();
 			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
-			ctx.lineTo(hexagonUnitHeight, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
-			ctx.lineTo(hexagonUnitHeight, 0);
-			ctx.stroke();
-			ctx.beginPath();
-			ctx.rotate((60 * Math.PI) / 180);
-			ctx.moveTo(0, 0);
+			ctx.moveTo(-hexagonUnitHeight, 0);
 			ctx.lineTo(hexagonUnitHeight, 0);
 			ctx.stroke();
 			ctx.restore();
@@ -469,13 +442,223 @@ l 0                   1
 l ${-hexagonUnitHeight}  0.5
 l ${-hexagonUnitHeight} -0.5
 z`);
+
+const mostCommonColor = (cells: Cells, span: Set<CoordinateKey>): Color => {
+	const colorCount: Record<Color, number> = {
+		none: 0,
+		red: 0,
+		green: 0,
+		blue: 0,
+	};
+	for (const coordinateKey of span) {
+		const { color } = cells[coordinateKey]!;
+		if (color) {
+			colorCount[color] = (colorCount[color] ?? 0) + 1;
+		}
+	}
+	const { color } = Object.entries(colorCount).reduce<{
+		color: Color;
+		count: number;
+	}>(
+		(highest, [color, count]) => {
+			if (highest.count > count) {
+				return highest;
+			}
+			return {
+				color: color as Color,
+				count,
+			};
+		},
+		{
+			color: "none",
+			count: 0,
+		}
+	);
+
+	return color;
+};
+function assertCoordinateKey(value: string): asserts value is CoordinateKey {}
+
+const entries = <K extends string, V>(values: Record<K, V>): [K, V][] =>
+	Object.entries(values) as [K, V][];
+
+const updateColors = (
+	cells: Cells,
+	facingNeighboursByCoordinate: Record<CoordinateKey, Set<CoordinateKey>>
+) => {
+	let nextSpan = 0;
+	const spanByCoordinate: Record<CoordinateKey, number> = {};
+	const floodFill = (coordinateKey: CoordinateKey, span: number) => {
+		if (spanByCoordinate[coordinateKey]) {
+			return;
+		}
+		spanByCoordinate[coordinateKey] = span;
+		const facingNeighbours = facingNeighboursByCoordinate[coordinateKey];
+		if (facingNeighbours) {
+			for (const neighbourKey of facingNeighbours) {
+				assertCoordinateKey(neighbourKey);
+				if (facingNeighboursByCoordinate[neighbourKey]?.has(coordinateKey)) {
+					floodFill(neighbourKey, span);
+				}
+			}
+		}
+	};
+	for (const coordinateKey of Object.keys(cells)) {
+		assertCoordinateKey(coordinateKey);
+		floodFill(coordinateKey, ++nextSpan);
+	}
+	const spans: Record<number, Set<CoordinateKey>> = {};
+	for (const [coordinateKey, span] of Object.entries(spanByCoordinate)) {
+		assertCoordinateKey(coordinateKey);
+		spans[span] ??= new Set<CoordinateKey>();
+		spans[span]!.add(coordinateKey);
+	}
+	if (Object.values(spans).length === 1) {
+		return console.log({
+			type: "game over",
+		});
+	}
+
+	const avaiableColors = new Set<Color>(Colors);
+	avaiableColors.delete("none");
+
+	const colors: Record<Color, Set<CoordinateKey>> = {
+		none: new Set<CoordinateKey>(),
+		red: new Set<CoordinateKey>(),
+		green: new Set<CoordinateKey>(),
+		blue: new Set<CoordinateKey>(),
+	};
+	const sortedSpans = Object.values(spans).sort((a, b) => b.size - a.size);
+	const longestSpans = [];
+	const uncoloredSpans = [];
+
+	for (const span of sortedSpans) {
+		if (span.size > 1 && longestSpans.length < avaiableColors.size) {
+			longestSpans.push(span);
+		} else {
+			uncoloredSpans.push(span);
+		}
+	}
+
+	for (const span of longestSpans) {
+		const currentColor = mostCommonColor(cells, span);
+		if (avaiableColors.has(currentColor)) {
+			colors[currentColor] = span;
+			avaiableColors.delete(currentColor);
+		} else {
+			uncoloredSpans.unshift(span);
+		}
+	}
+
+	let i = 0;
+	for (const color of avaiableColors) {
+		if (i >= uncoloredSpans.length) {
+			break;
+		}
+		const span = uncoloredSpans[i]!;
+		if (span.size <= 1) {
+			break;
+		}
+		colors[color] = span;
+		i += 1;
+	}
+
+	for (const span of uncoloredSpans.slice(i)) {
+		for (const coodrinateKey of span) {
+			colors.none.add(coodrinateKey);
+		}
+	}
+
+	for (const [color, coordinateKeys] of entries(colors)) {
+		for (const coordinateKey of coordinateKeys) {
+			cells[coordinateKey]!.color = color;
+		}
+	}
+};
+const rotate = (
+	coordinate: Coordinate,
+	orientation: Orientation,
+	origin: Coordinate
+): Coordinate => {
+	coordinate.q -= origin.q;
+	coordinate.r -= origin.r;
+	coordinate.s -= origin.s;
+	while (orientation > 0) {
+		const next = {
+			q: -coordinate.r,
+			r: -coordinate.s,
+			s: -coordinate.q,
+		};
+		coordinate = next;
+		orientation -= 60;
+	}
+	coordinate.q += origin.q;
+	coordinate.r += origin.r;
+	coordinate.s += origin.s;
+	return coordinate;
+};
+
+function* Neighbours({
+	coordinate,
+	orientation: { value: orientation },
+	connection,
+}: Cell): Generator<Coordinate> {
+	const { q, r, s } = coordinate;
+	// r
+	if (connection & 0b100000) {
+		yield rotate({ q: q + 1, r, s: s - 1 }, orientation, coordinate);
+	}
+
+	// -q
+	if (connection & 0b010000) {
+		yield rotate({ q, r: r + 1, s: s - 1 }, orientation, coordinate);
+	}
+
+	// s
+	if (connection & 0b001000) {
+		yield rotate({ q: q - 1, r: r + 1, s }, orientation, coordinate);
+	}
+
+	// -r
+	if (connection & 0b000100) {
+		yield rotate({ q: q - 1, r, s: s + 1 }, orientation, coordinate);
+	}
+
+	// q
+	if (connection & 0b000010) {
+		yield rotate({ q, r: r - 1, s: s + 1 }, orientation, coordinate);
+	}
+
+	// -s
+	if (connection & 0b000001) {
+		yield rotate({ q: q + 1, r: r - 1, s }, orientation, coordinate);
+	}
+}
+
+const updateFacingNeighbours = (
+	coordinateKey: CoordinateKey,
+	cells: Cells,
+	facingNeighboursByCoordinate: Record<CoordinateKey, Set<CoordinateKey>>
+) => {
+	const cell = cells[coordinateKey]!;
+	for (const neighbour of Neighbours(cell)) {
+		const neighbourKey = asCoordinateKey(neighbour);
+		const neighbourCell = cells[neighbourKey];
+		if (!neighbourCell) {
+			continue;
+		}
+		facingNeighboursByCoordinate[coordinateKey] ??= new Set<CoordinateKey>();
+		facingNeighboursByCoordinate[coordinateKey]!.add(neighbourKey);
+	}
+};
+
 const GameLoop =
 	(memory: Memory) =>
 	(time: DOMHighResTimeStamp, [width, height]: Dimension, events: Event[]) => {
 		ctx.fillStyle = background;
 		ctx.fillRect(0, 0, width, height);
 
-		const size = 1;
+		const size = 5;
 		if (!memory["state"]) {
 			const cells: Cells = {};
 			const config = {
@@ -495,10 +678,22 @@ const GameLoop =
 						duration: 250,
 					},
 					connection,
+					color: "none",
 				};
 			}
 			memory["cells"] = cells;
 			memory["state"] = "playing";
+			const facingNeighboursByCoordinate = {};
+			for (const coordinateKey of Object.keys(cells)) {
+				assertCoordinateKey(coordinateKey);
+				updateFacingNeighbours(
+					coordinateKey,
+					cells,
+					facingNeighboursByCoordinate
+				);
+			}
+			memory["facingNeighboursByCoordinate"] = facingNeighboursByCoordinate;
+			updateColors(cells, facingNeighboursByCoordinate);
 		}
 
 		switch (memory["state"]) {
@@ -510,7 +705,8 @@ const GameLoop =
 				const cells: Cells = memory["cells"];
 				ctx.fillStyle = cellBackground;
 				const pointerDown = events.find(({ type }) => type === "pointerdown");
-				for (const cell of Object.values(cells)) {
+				for (const [coordinateKey, cell] of Object.entries(cells)) {
+					assertCoordinateKey(coordinateKey);
 					const {
 						coordinate: { q, r },
 					} = cell;
@@ -526,6 +722,16 @@ const GameLoop =
 						cell.orientation.value %= 360;
 						cell.orientation.animate = "clockwise";
 						cell.orientation.startTime = time;
+
+						const facingNeighboursByCoordinate =
+							memory["facingNeighboursByCoordinate"];
+						delete facingNeighboursByCoordinate[coordinateKey];
+						updateFacingNeighbours(
+							coordinateKey,
+							cells,
+							facingNeighboursByCoordinate
+						);
+						updateColors(cells, facingNeighboursByCoordinate);
 						draw();
 					}
 					ctx.restore();
